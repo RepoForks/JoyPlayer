f5903c45ece06a7181e056479ffa8eb899178628
Shivam Satija <dr.droid27@gmail.com>
2017-06-21 16:52:21 +0530

    Dagger dependencies added

---
+++ app/src/main/java/developer/shivam/joyplayer/BaseApplication.kt
@@ -0,0 +1,15 @@
+package developer.shivam.joyplayer
+
+import android.app.Application
+
+/*
+ * Created by shivam on 21/6/17.
+ */
+class BaseApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+}
