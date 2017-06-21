c27b4bd84643003948c6b1b9af93171f1d291806
Shivam Satija <dr.droid27@gmail.com>
2017-06-22 00:53:15 +0530

    Navigation darwer added

---
+++ app/src/main/java/developer/shivam/joyplayer/core/Session.kt
@@ -0,0 +1,8 @@
+package developer.shivam.joyplayer.core
+
+/*
+ * Created by shivam on 22/6/17.
+ */
+class Session {
+
+}
--- app/src/main/java/developer/shivam/joyplayer/ui/home/HomeActivity.kt
+++ app/src/main/java/developer/shivam/joyplayer/ui/home/HomeActivity.kt
@@ -1,23 +1,32 @@
 package developer.shivam.joyplayer.ui.home
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.Toolbar
 import android.widget.Button
 import android.widget.Toast
 import developer.shivam.joyplayer.R
 import developer.shivam.joyplayer.ui.BaseActivity
 import javax.inject.Inject
 
-class HomeActivity: BaseActivity(), HomeMvpView {
+class HomeActivity : BaseActivity(), HomeMvpView {
 
     @Inject
     lateinit var presenter: HomePresenter<HomeMvpView>
 
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navigationView: NavigationView
+    lateinit var toolbar: Toolbar
     lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
         init()
+        setupNavigationDrawer()
 
         getComponent()?.inject(this)
         presenter.setMvpView(this)
@@ -28,7 +37,26 @@ class HomeActivity: BaseActivity(), HomeMvpView {
     }
 
     fun init() {
+        drawerLayout = findViewById(R.id.drawer_layout) as DrawerLayout
+        navigationView = findViewById(R.id.navigation_view) as NavigationView
+
+        toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
         button = findViewById(R.id.btn_fetch_songs) as Button
+    }
+
+    fun setupNavigationDrawer() {
+        var actionBarDrawerToggle: ActionBarDrawerToggle = ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.open,
+                R.string.close
+        )
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
     }
 
     override fun setSongsList() {
